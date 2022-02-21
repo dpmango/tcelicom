33f2f75feb83d23451a9603dfe7fb4f15215856e
Vyacheslav <vyacheslavlutsenkoml@gmail.com>
2022-02-22 00:42:20 +0500

    scoll effect

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -6,7 +6,38 @@
     init: function (fromPjax) {
       this.homeScrollListener();
     },
+
     homeScrollListener: function () {
+
+
+      function throttle (callee, timeout) {
+        let timer = null;
+        return function perform(...args) {
+          if (timer) return;
+
+          timer = setTimeout(() => {
+            callee(...args);
+            clearTimeout(timer);
+            timer = null;
+          }, timeout);
+        };
+      }
+
+      const scrollListener = throttle(scroll, 50)
+
+      function scroll() {
+        const pageHeight = $('body').innerHeight()
+        const sectionHeight = pageHeight / 5
+
+
+        const el = Math.round($(window).scrollTop() / sectionHeight)
+        console.log(el);
+        $(`.home__screen:eq(${el})`).addClass('is-active')
+
+      }
+
+      $(window).on('scroll', scrollListener)
+
     },
   };
 })(jQuery, window.APP);
